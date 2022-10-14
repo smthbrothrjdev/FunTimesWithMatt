f1f2ff4a53ba9e9ee79ec84b569dd9c35c06555e
Action Jackson <smthbrothrjdev@gmail.com>
2022-10-13 19:15:02 -0500

    update look and implement sendEmail

--- mock-gmail/src/SendEmailBox.js
+++ mock-gmail/src/SendEmailBox.js
@@ -4,9 +4,18 @@ import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 export function SendEmailBox({ redo }) {
+  const blankEmail = {
+    sender: "",
+    recipient: "jane@Galvanize.com",
+    subject: "",
+    message: "",
+  };
   const [flag, setFlag] = useState(false);
   const [fetchie, setFetchie] = useState({});
   const [visibleFlag, setVisibleFlag] = useState(false);
+  const [buttonText, setButtonText] = useState("Compose");
+  const [emailComposer, setEmailComposer] = useState(blankEmail);
+
   useEffect(() => {
     if (flag) {
       fetch("http://localhost:3001/send", {
@@ -15,28 +24,37 @@ export function SendEmailBox({ redo }) {
           Accept: "application/json",
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          sender: "jane@Galvanize.com",
-          recipient: "YourBoss@Boss.Boss",
-          subject: "YOU ARE FIRED",
-          message: "CHINA",
-        }),
+        body: JSON.stringify(emailComposer),
       }).finally((res) => {
         redo();
+        setEmailComposer(blankEmail);
       });
     } else {
     }
-  }, [fetchie, redo, flag]);
+  }, [fetchie, flag]);
 
   function handleClick() {
     setFlag(true);
     setFetchie({ run: true });
   }
 
+  function handleChange(target) {
+    setEmailComposer((prevState) => ({
+      ...prevState,
+      [target.id]: target.value,
+    }));
+  }
+
   return (
     <Box sx={{ display: "flex", flexDirection: "column" }}>
-      <Button variant="contained" onClick={() => setVisibleFlag(true)}>
-        Compose
+      <Button
+        variant="contained"
+        onClick={() => {
+          setVisibleFlag(!visibleFlag);
+          setButtonText(!visibleFlag === true ? "Hide" : "Compose");
+        }}
+      >
+        {buttonText}
       </Button>
       <Box
         sx={{
@@ -46,11 +64,41 @@ export function SendEmailBox({ redo }) {
           m: 1,
         }}
       >
-        <TextField id="to" label="TO" variant="outlined" />
-        <TextField id="subject" label="SUBJECT" variant="outlined" />
-        <TextField id="message" label="MESSAGE" variant="outlined" />
+        <TextField
+          id="sender"
+          label="To"
+          variant="outlined"
+          value={emailComposer.sender}
+          onChange={(e) => handleChange(e.target)}
+        />
+        <TextField
+          id="subject"
+          label="Subject"
+          variant="outlined"
+          value={emailComposer.subject}
+          onChange={(e) => handleChange(e.target)}
+        />
+        <TextField
+          id="message"
+          label="Message"
+          variant="outlined"
+          value={emailComposer.message}
+          onChange={(e) => handleChange(e.target)}
+        />
 
-        <button onClick={() => handleClick()}>HELLO THERE</button>
+        <Button
+          variant="contained"
+          disabled={
+            !(
+              emailComposer.message &&
+              emailComposer.sender &&
+              emailComposer.subject
+            )
+          }
+          onClick={() => handleClick()}
+        >
+          send
+        </Button>
       </Box>
     </Box>
   );
